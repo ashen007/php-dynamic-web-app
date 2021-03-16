e0ffc231821e532449f2ce430d26c1cb0ea5f3df
ashen007 <asheniranga@gmail.com>
2021-03-16 22:13:55 +0530

    create global path parameter to easy access in any where

--- private/initialize.php
+++ private/initialize.php
@@ -1,4 +1,9 @@
 <?php
+//define globel path
+define("PRIVATE_PATH", dirname(__FILE__));
+define("PROJECT_PATH", dirname(PRIVATE_PATH));
+define("PUBLIC_PATH", PROJECT_PATH.'/public');
+define("SHARED_PATH", PRIVATE_PATH.'/shared');
 
 require_once('functions.php');
 

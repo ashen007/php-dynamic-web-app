3a27b0b627b634ab3c4fe8e7fc0716e5a222167b
ashen007 <asheniranga@gmail.com>
2021-03-16 23:00:03 +0530

    create function to generate global paths

--- private/functions.php
+++ private/functions.php
@@ -1,1 +1,12 @@
 <?php
+
+// generate correct html paths for sources
+function url_for($script_path)
+{
+    if ($script_path[0] != '/') {
+        $script_path = "/" . $script_path;
+    }
+    return WWW_ROOT . $script_path;
+}
+
+?>

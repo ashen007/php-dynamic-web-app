c278c36d5a315b2e7b2b864100f232b533471ba1
ashen007 <asheniranga@gmail.com>
2021-03-16 21:31:53 +0530

    add basic layout to member page including navigation bar, header and footer

--- public/members/index.php
+++ public/members/index.php
@@ -1,1 +1,24 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Crop Club</title>
+</head>
+<body>
+<header>
+    <h1>Crop Club</h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="index.php">Menu</a></li>
+    </ul>
+</nav>
+<footer>
+    &copy;<?php echo date('Y'); ?> CropFoundation
+</footer>
+</body>
+</html>
+

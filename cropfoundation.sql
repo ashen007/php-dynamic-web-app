fd426a1bee09528e86262ca5bcddb7a0f3b10a3a
ashen007 <asheniranga@gmail.com>
2021-03-18 20:59:37 +0530

    add user

--- cropfoundation.sql
+++ cropfoundation.sql
@@ -1,13 +1,18 @@
+# create database for crop foundation web app
 create database crop_foundation;
-
 use crop_foundation;
 
+# create member privileges table
 create table member_privilege(
     id int not null,
     name varchar(32),
     primary key (id)
 );
 
+# create member table
+# this table consists members account details
+# password encrypted via php program so here
+# didn't use any encrypted column
 create table members(
     id int not null auto_increment,
     firstname varchar(32) not null,
@@ -19,3 +24,9 @@ create table members(
     create_at timestamp default current_timestamp,
     primary key (id,email)
 );
+
+# create special user client for mysql server to
+# handle web app connections
+# to execute this section you need admin privileges
+create user if not exists wt_bridge@localhost;
+grant select,insert,update,delete on crop_foundation.* to wt_bridge@localhost;

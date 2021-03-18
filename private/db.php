e96e24121a0902aed19f4908e00df45e990328ca
ashen007 <asheniranga@gmail.com>
2021-03-18 19:51:49 +0530

    connection error handler

--- private/db.php
+++ private/db.php
@@ -6,7 +6,19 @@ require_once('cred.php');
 function db_connect()
 {
     $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_SCHEMA);
+    check_db_connection();
     return $connection;
+}
+
+// check connection status
+function check_db_connection()
+{
+    if (mysqli_connect_errno()) {
+        $msg = 'Connection faild: ';
+        $msg .= mysqli_connect_error();
+        $msg .= '(' . mysqli_connect_errno() . ')';
+        exit($msg);
+    }
 }
 
 // close the conection with database

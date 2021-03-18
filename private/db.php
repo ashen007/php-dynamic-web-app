86c61cdcb65d047d08191e3a894ef2a96f106432
ashen007 <asheniranga@gmail.com>
2021-03-18 17:56:55 +0530

    custom functions to connect to database and close the connection

--- private/db.php
+++ private/db.php
@@ -2,4 +2,19 @@
 
 require_once('cred.php');
 
+// make a connection with database
+function db_connect()
+{
+    $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_SCHEMA);
+    return $connection;
+}
+
+// close the conection with database
+function db_close($connection)
+{
+    if (isset($connection)) {
+        mysqli_close($connection);
+    }
+}
+
 ?>

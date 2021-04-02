12fcf6b4e434b788b4d7bc1a9eb6c9a7f7e401ca
ashen007 <asheniranga@gmail.com>
2021-04-02 14:10:50 +0530

    complete landing area

--- public/index.php
+++ public/index.php
@@ -9,6 +9,49 @@ $page_title = 'Crop Foundation';
 <!--call header hook-->
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
+<section id="public_landing">
+    <div class="wrapper container">
+        <div class="landing_bg">
+            <ul class="cliper">
+                <li class="board"></li>
+                <li class="board"></li>
+                <li class="board"></li>
+                <li class="board"></li>
+            </ul>
+        </div>
+        <div class="moto valign">
+            <div class="moto_content">
+                <div class="public_header valign">
+                    <div><h1>crop foundation</h1></div>
+                </div>
+                <div class="public_sub_header valign">
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                    <h2>community of modern farmers</h2>
+                </div>
+            </div>
+            <div class="land_img">
+                <img src="<?php echo url_for('/images/public_landing.png') ?>">
+            </div>
+        </div>
+    </div>
+</section>
+
 
 <!--call footer hook-->
 <?php include(SHARED_PATH . '/public_footer.php'); ?>

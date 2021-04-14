20b3940cf79ba95f45abdf7b2ed1c789919f1e3a
ashen007 <asheniranga@gmail.com>
2021-04-14 10:15:03 +0530

    blog showcase structure created

--- public/blog.php
+++ public/blog.php
@@ -11,5 +11,44 @@ $page_title = 'Crop Foundation';
 
 <!--body-->
 
+<section class="landing_bg bottom_gap">
+    <div class="event_land_img"
+         style="background-image: url('<?php echo url_for('/images/precision_agriculture.jpg'); ?>')">
+        <ul class="landing_header valign">
+            <li class="header_content">title</li>
+            <li class="header_content">sub title</li>
+            <li class="header_content">
+                <p>moto</p>
+            </li>
+        </ul>
+    </div>
+</section>
+
+<section class="blog_posts bottom_gap">
+    <div class="blog_wrapper container">
+        <ul class="blog_filter"></ul>
+        <ul class="blog_cards">
+            <li class="blog_col">
+                <ul class="blog_row">
+                    <li>card</li>
+                    <li>card</li>
+                </ul>
+            </li>
+            <li class="blog_col">
+                <ul class="blog_row">
+                    <li>card</li>
+                    <li>card</li>
+                </ul>
+            </li>
+            <li class="blog_col">
+                <ul class="blog_row">
+                    <li>card</li>
+                    <li>card</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</section>
+
 <!--call footer hook-->
 <?php include(SHARED_PATH . '/public_footer.php'); ?>

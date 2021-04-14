9a1922594e49d4355f3d5523c29236b36623ab31
ashen007 <asheniranga@gmail.com>
2021-04-14 09:09:18 +0530

    add navigation and footer to all public area pages

--- public/events.php
+++ public/events.php
@@ -1,1 +1,18 @@
+<!--don't change this path to dynamicly genarate!!!!!-->
+<?php require_once('../private/initialize.php'); ?>
+
+<!--page title-->
 <?php
+$page_title = 'Crop Foundation';
+?>
+
+<!--call header hook-->
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
+<!--body-->
+<section class="landing_bg">
+    <div class="event_land_img" style="background-image: url('<?php echo url_for('/images/Agricultural-Heritage-scaled.jpg');?>')"></div>
+</section>
+
+<!--call footer hook-->
+<?php include(SHARED_PATH . '/public_footer.php'); ?>

e0fb0774e9f8d0d99bb53bb0cdf38d62da075631
ashen007 <asheniranga@gmail.com>
2021-04-14 10:49:38 +0530

    about page basic structure

--- public/about.php
+++ public/about.php
@@ -11,5 +11,33 @@ $page_title = 'Crop Foundation';
 
 <!--body-->
 
+<section class="landing_bg bottom_gap">
+    <div class="event_land_img"
+         style="background-image: url('<?php echo url_for('/images/tumblr_mdo83xsqnv1rn3wcfo1_1280.jpg'); ?>')">
+        <ul class="landing_header valign">
+            <li class="header_content">title</li>
+            <li class="header_content">sub title</li>
+            <li class="header_content">
+                <p>moto</p>
+            </li>
+        </ul>
+    </div>
+</section>
+
+<section class="about_info top_gap bottom_gap">
+    <div class="about_wrapper">
+        <ul class="about_content_wrapper">
+            <li class="about_content">
+                <div>
+                    <h2>title</h2>
+                    <h4>moto</h4>
+                    <p>text</p>
+                </div>
+            </li>
+            <li class="about_content">img</li>
+        </ul>
+    </div>
+</section>
+
 <!--call footer hook-->
 <?php include(SHARED_PATH . '/public_footer.php'); ?>

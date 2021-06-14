56ab90e6372b35e0dd60f348ac510b3315b2933e
ashen007 <asheniranga@gmail.com>
2021-06-14 21:42:10 +0530

    create product table

--- database/cropfoundation.sql
+++ database/cropfoundation.sql
@@ -1,10 +1,10 @@
 # create database for crop foundation web app
 create database crop_foundation;
-use crop_foundation;
 
 # create member privileges table
-create table member_privilege(
-    id int not null,
+create table crop_foundation.member_privilege
+(
+    id   int not null,
     name varchar(32),
     primary key (id)
 );
@@ -13,30 +13,46 @@ create table member_privilege(
 # this table consists members account details
 # password encrypted via php program so here
 # didn't use any encrypted column
-create table members(
-    id int not null auto_increment,
-    firstname varchar(32) not null,
-    lastname varchar(64) not null,
-    username varchar(255) not null unique,
-    email varchar(255) not null unique,
-    password varchar(255) not null,
-    pp blob null,
+create table crop_foundation.members
+(
+    id        int          not null auto_increment,
+    firstname varchar(32)  not null,
+    lastname  varchar(64)  not null,
+    username  varchar(255) not null unique,
+    email     varchar(255) not null unique,
+    password  varchar(255) not null,
+    pp        blob         null,
     create_at timestamp default current_timestamp,
-    primary key (id,email)
+    primary key (id, email)
 );
 
-create table headlines(
-    head_id int not null auto_increment,
-    head_body varchar(128) not null,
+create table crop_foundation.headlines
+(
+    head_id    int          not null auto_increment,
+    head_body  varchar(128) not null,
     created_at timestamp default current_timestamp,
     primary key (head_id)
 );
 
-alter table members add column dob timestamp null;
-alter table members change column pp pp longblob null;
+alter table crop_foundation.members
+    add column dob timestamp null;
+alter table crop_foundation.members
+    change column pp pp longblob null;
+
+# create product detail table
+create table crop_foundation.price
+(
+    id       int          not null auto_increment,
+    category varchar(255) not null,
+    item     varchar(255) not null,
+    unit     varchar(255) not null,
+    price    double(5, 2) not null,
+
+    primary key (id)
+);
 
 # create special user client for mysql server to
 # handle web app connections
 # to execute this section you need admin privileges
-create user if not exists wt_bridge@localhost;
-grant select,insert,update,delete on crop_foundation.* to wt_bridge@localhost;
+create user if not exists wt_bridge@localhost identified by 'wt2021#cis';
+grant select, insert, update, delete on crop_foundation.* to wt_bridge@localhost;

bffda2131b4277ff84445ef4c6cd5b531b8dbdeb
ashen007 <asheniranga@gmail.com>
2021-07-10 12:32:59 +0530

    create shared show page for all 3rd level links to share target contents

--- private/cred.php
+++ private/cred.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_SERVER', 'us-cdbr-east-04.cleardb.com');
-define('DB_USER', 'b161c79fcd5530');
-define('DB_PASS', '55380468');
-define('DB_SCHEMA', 'heroku_3624d57306115b6');
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'wt_bridge');
+define('DB_PASS', '1234567890');
+define('DB_SCHEMA', 'crop_foundation');
 
 ?>
--- shared/show.php
+++ shared/show.php
@@ -2,7 +2,7 @@
 <?php
 $page = xss(base64_decode($_GET['page']));
 $id = xss(base64_decode($_GET['item']));
-$content = get_content($page, $id);
+$content = mysqli_fetch_assoc(get_content($page, $id));
 ?>
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
@@ -10,9 +10,41 @@ $content = get_content($page, $id);
 <?php } ?>
 
 <?php if ($page == 'events') { ?>
+    <section class="landing_bg bottom_gap">
+        <div class="event_land_img"
+             style="background-image: url('data:image/jpg;base64,<?php echo base64_encode($content["event_banner"]); ?>');">
+            <ul class="landing_header valign">
+                <li class="header_content"><h1><?php echo $content['headline']; ?></h1></li>
+                <li class="header_content"><h2><?php echo $content['location']; ?></h2></li>
+                <li class="header_content">
+                    <p><?php echo $content['cover_by']; ?></p>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="event_body">
+        <?php echo $content['content']; ?>
+    </section>
 <?php } ?>
 
 <?php if ($page == 'blog') { ?>
+    <section class="landing_bg bottom_gap">
+        <div class="event_land_img"
+             style="background-image: url('data:image/jpg;base64,<?php echo base64_encode($content["post_banner"]); ?>');">
+            <ul class="landing_header valign">
+                <li class="header_content"><h1><?php echo $content['post_headline']; ?></h1></li>
+                <li class="header_content"><h2><?php echo $content['publish_at']; ?></h2></li>
+                <li class="header_content">
+                    <p><?php echo $content['author']; ?></p>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="event_body">
+        <?php echo $content['content']; ?>
+    </section>
 <?php } ?>
 
 <?php include(SHARED_PATH . '/public_footer.php'); ?>

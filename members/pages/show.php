bdee78e8c0b749eb860571441e3f03e068dda00b
ashen007 <asheniranga@gmail.com>
2021-08-02 17:35:15 +0530

    add member are show page

--- members/pages/show.php
+++ members/pages/show.php
@@ -1,1 +1,13 @@
+<!--don't change this path to dynamicly genarate!!!!!-->
+<?php require_once('../../private/initialize.php'); ?>
+
+<!--call header hook-->
+<?php include(SHARED_PATH . '/profile_header.php'); ?>
 <?php
+$id = $_GET['id'];
+$page = $_GET['page']
+?>
+
+
+<!--call footer hook-->
+<?php include(SHARED_PATH . '/profile_footer.php'); ?>
